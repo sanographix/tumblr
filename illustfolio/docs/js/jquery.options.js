580c38d123626b4475e10755999f26140e7b7881
sanographix <mail@sanographix.net>
2012-12-09 20:45:31 +0900

    github page追加

---
+++ illustfolio/docs/js/jquery.options.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    // flexslider
+    $('.flexslider').flexslider({
+      animation: "slide",
+      controlNav: true,
+      animationLoop: true,
+      slideshow: true,
+      slideshowSpeed: 6000
+    });
+
+});
