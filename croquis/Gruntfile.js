12a45ef50dc6589a0dd1e0229c189607b42d0318
sanographix <mail@sanographix.net>
2014-01-01 13:54:08 +0900

    croquis page

---
+++ croquis/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt){
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.initConfig({
+
+        less : {
+            // 作業のラベルは"dist"にしてみます
+            dist : {
+                // オプションの指定
+                options : {
+                    compress : true
+                },
+                // コンパイルするファイルの指定。左辺には出力先のCSSファイル、右辺には元となるLessファイルへのパス
+                files : {
+                    "css/style.css" : "css/style.less"
+                }
+            }
+        },
+
+        watch : {
+            // ラベルは"less"にしてみます
+            less : {
+                // "files"セクションで監視するファイルの条件を指定
+                files : [
+                    "css/*.less"
+                ],
+                // "tasks"セクションで実行するタスクを指定
+                tasks : [
+                    "less:dist"
+                ]
+            }
+        }
+
+    });
+
+};
