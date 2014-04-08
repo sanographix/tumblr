fc16418b7c64433146b5f02f7cfd42e7c9c72c9f
sanographix <mail@sanographix.net>
2014-04-08 13:54:22 +0900

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt){
                 },
                 // コンパイルするファイルの指定。左辺には出力先のCSSファイル、右辺には元となるLessファイルへのパス
                 files : {
-                    "croquis/css/style.css" : "croquis/css/style.less"
+                    "croquis/css/style.css" : "croquis/css/style.less",
+                    "css/style.css" : "css/style.less"
                 }
             }
         },
@@ -24,7 +25,10 @@ module.exports = function(grunt){
                 // "files"セクションで監視するファイルの条件を指定
                 files : [
                     "*/*.less",
-                    "*/*/*.less"
+                    "*/*/*.less",
+                    "*/*/*/*.less",
+                    "*/*/*/*/*.less",
+                    "*/*/*/*/*/*.less"
                 ],
                 // "tasks"セクションで実行するタスクを指定
                 tasks : [
