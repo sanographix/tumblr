081e8ecb380679b5881a5efc31d2cbedd14d9d8d
sanographix <mail@sanographix.net>
2017-06-18 17:37:20 +0900

    画像のディレクトリを変更

---
+++ build/js/scripts.js
@@ -0,0 +1,1 @@
+!function(){for(var t=document.querySelectorAll("a"),e=0;e<t.length;e++)t[e].host!==location.host&&(t[e].setAttribute("target","_blank"),t[e].setAttribute("rel","noopener noreferrer"))}();
