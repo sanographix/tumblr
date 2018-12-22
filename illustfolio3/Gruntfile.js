11b2c4e569491d404f36bb2fdb4e71bd6289224d
sanographix <mail@sanographix.net>
2018-12-22 12:13:07 +0900

    update case

--- illustfolio3/Gruntfile.js
+++ illustfolio3/Gruntfile.js
@@ -2,7 +2,6 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-pngmin');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-connect');
@@ -34,21 +33,6 @@ module.exports = function(grunt){
                     dest: '<%= paths.imgdist %>'
                 }]
             }
-        },
-
-        // pngmin
-        pngmin: {
-          compile: {
-            options: {
-              ext: '.png'
-            },
-            files: [{
-              expand: true,
-              cwd: '<%= paths.img %>',
-              src: '**/*.png',
-              dest: '<%= paths.imgdist %>'
-            }]
-          }
         },
 
         // js-concat
