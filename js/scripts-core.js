143bb2df7c33ac678b87a69ba932bc546f05881c
sanographix <mail@sanographix.net>
2020-12-27 17:55:51 +0900

    ドメイン除外

--- js/scripts-core.js
+++ js/scripts-core.js
@@ -2,7 +2,7 @@
 (function() {
   var a = document.querySelectorAll('a');
   for (var i = 0; i < a.length; i++) {
-    if (a[i].host !== location.host) {
+    if (a[i].host !== location.host && a[i].host !== 'sanographix.net' && a[i].host !== 'text.sanographix.net') {
       a[i].setAttribute('target', '_blank');
       a[i].setAttribute('rel', 'noopener noreferrer');
     }
